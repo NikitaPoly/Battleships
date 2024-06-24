00eb59ed8ea429b544fb6a0c035f001e8a27f27c
NikitaPoly <nikitapolytech@gmail.com>
2024-06-23 21:15:14 -0400

    added ships

--- scenes/GridContainer.gd
+++ scenes/GridContainer.gd
@@ -1,22 +1,26 @@
 extends GridContainer
 
 @export var landProbobility : float = 30
+@export var shipProbobility : float = 30
 var rng = RandomNumberGenerator.new()
 
 func _ready():
 	set_all_tiles()
 
 func set_all_tiles():
-	print(columns)
-	for row in range(columns):
-		for column in range(columns):
-			var newTile :ColorRect = get_water_or_land()
+	for x in range(columns):
+		for y in range(columns):
+			var newTile = get_water_or_land()
+			newTile.name = str(x) + str(y)
 			newTile.visible = true
 			add_child(newTile)
 			
 func get_water_or_land():
 	var percent = rng.randf_range(0, 100)
 	if percent <= 30:
+		percent = rng.randf_range(0, 100)
+		if percent <= shipProbobility:
+			return $ShipTile.duplicate()
 		return $LandTile.duplicate()
 	else:
 		return $WaterTile.duplicate()
